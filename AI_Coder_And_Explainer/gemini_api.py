b22269fdebb3a99915a406698f598ec9d1cdaba8
MuneebMalik244535 <aniqayan2@gmail.com>
2025-04-28 02:00:41 +0500

    Update gemini_api.py

--- AI_Coder_And_Explainer/gemini_api.py
+++ AI_Coder_And_Explainer/gemini_api.py
@@ -1,24 +1,3 @@
-# import google.generativeai as genai
-# import os
-# from dotenv import load_dotenv
-
-
-# genai.configure(api_key="AIzaSyBJnLpWyU94PCrjB4ohXGicDt8yfERDLTc")
-
-# def get_fixed_code(code, explain=False):
-#     with open("prompt_template.txt", "r") as f:
-#         prompt = f.read()
-
-#     prompt = prompt.replace("<CODE_HERE>", code)
-
-#     if not explain:
-#         prompt = prompt.split("3.")[0] + f"\n\nHere is the code:\n{code}"
-
-#     model = genai.GenerativeModel("gemini-1.5-pro")
-#     response = model.generate_content(prompt)
-#     return response.text
-
-
 
 import google.generativeai as genai
 import os
